7b639088b4885aadece41dae46594d0977009141
Michael Pezzi <mike@thespiral.ca>
2011-11-21 15:42:40 -0500

    Added slide callback

--- collapse.js
+++ collapse.js
@@ -2,7 +2,28 @@
  * Implements Drupal.behaviors
  */
 Drupal.behaviors.collapse = function() {
-  jQuery.each(Drupal.settings.collapse, function(selector, options){
+  jQuery.each(Drupal.settings.collapse, function(selector, options) {
+    var effects = Drupal.behaviors.collapse.effects;
+
+    // Set show as callback.
+    if ( typeof options.show == 'string' && typeof effects[options.show] !== 'undefined' ) {
+      options.show = effects[options.show];
+    }
+
+    // Set hide as a callback.
+    if ( typeof options.hide == 'string' && typeof effects[options.hide] !== 'undefined' ) {
+      options.hide = effects[options.hide];
+    }
+
     $('.' + selector).collapse(options);
   });
+};
+
+Drupal.behaviors.collapse.effects = {
+  slide: function() {
+    this.animate({
+      opacity: 'toggle',
+      height: 'toggle',
+    }, 250);
+  }
 };
